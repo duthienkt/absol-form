37c2dcff93521dd8442897eec27a49d36ae9e9c6
duthienkt <blueskythien2010@live.com>
2019-09-20 17:49:07 +0700

    - export more

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import './css/base.css'
+import './css/layouteditor.css'
+
 import RelativeAnchor from './js/anchors/RelativeAnchor'
 import TextInput from './js/components/TextInput'
 import RelativeLayout from './js/layouts/RelativeLayout'
 import Assembler from './js/core/Assembler'
 import DateInput from './js/components/DateInput'
 import BaseComponent from './js/core/BaseComponent'
+import LayoutEditor from './js/editor/LayoutEditor'
+import Fcore from './js/core/FCore'
 
 export default {
+    LayoutEditor: LayoutEditor,
     RelativeAnchor:RelativeAnchor,
     BaseComponent: BaseComponent,
     TextInput:TextInput,
     RelativeLayout: RelativeLayout,
     Assembler: Assembler,
-    DateInput: DateInput
+    DateInput: DateInput,
+    core: Fcore
 };
