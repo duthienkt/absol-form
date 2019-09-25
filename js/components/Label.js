b522120090c65973295dd1fde01d65974c20b036
duthienkt <blueskythien2010@live.com>
2019-09-25 12:00:34 +0700

    - new label

--- js/components/Label.js
+++ js/components/Label.js
@@ -1,16 +1,16 @@
 import Fcore from "../core/FCore";
 
 import '../../css/component.css';
-import ScalableComponent from "../core/ScalableComponent";
+import ContentScalelessComponent from "../core/ContentScalelessComponent";
 
 var _ = Fcore._;
 var $ = Fcore.$;
 
 function Label() {
-    ScalableComponent.call(this);
+    ContentScalelessComponent.call(this);
 }
 
-Object.defineProperties(Label.prototype, Object.getOwnPropertyDescriptors(ScalableComponent.prototype));
+Object.defineProperties(Label.prototype, Object.getOwnPropertyDescriptors(ContentScalelessComponent.prototype));
 Label.prototype.constructor = Label;
 
 Label.prototype.tag = "Label";
@@ -20,37 +20,13 @@ Label.prototype.SUPPORT_ATTRIBUTE_NAMES = ['value'];
 Label.prototype.SUPPORT_EVENT_NAMES = ['change'];
 
 
-Label.prototype.onCreated = function () {
-    this.$label = $('label', this.view);
-    this.$cell = $('.as-comp-label-cell', this.view);
-    ScalableComponent.prototype.onCreated.call(this);
-};
-
-Label.prototype.render = function () {
-    return _({
-        class: 'as-comp-label',
-
-        child: {
-            class: 'as-comp-label-cell',
-            child: {
-                tag: 'label'
-            }
-        }
-    });
-};
-
-Label.prototype.handleStyleTextHAlign = function (value) {
-    this.view.addStyle('text-align', value);
-};
-
-Label.prototype.handleStyleTextVAlign = function (value) {
-    if (value == 'center') value = 'middle';
-    this.$cell.addStyle('vertical-align', value);
+Label.prototype.renderContent = function () {
+    return _('label');
 };
 
 
 Label.prototype.handleAttributeText = function (value) {
-    this.$label.clearChild().addChild(_({ text: value }))
+    this.$content.clearChild().addChild(_({ text: value }))
 };
 
 export default Label;
