9d86606445333a7d10a47bb7a3bed0c98d069124
duthienkt <blueskythien2010@live.com>
2019-09-25 11:59:59 +0700

    - ContentScalelessComponent

---
+++ js/core/ContentScalelessComponent.js
@@ -0,0 +1,50 @@
+import Fcore from "../core/FCore";
+
+import '../../css/component.css';
+import ScalableComponent from "../core/ScalableComponent";
+
+var _ = Fcore._;
+var $ = Fcore.$;
+
+function ContentScalelessComponent() {
+    ScalableComponent.call(this);
+}
+
+Object.defineProperties(ContentScalelessComponent.prototype, Object.getOwnPropertyDescriptors(ScalableComponent.prototype));
+ContentScalelessComponent.prototype.constructor = ContentScalelessComponent;
+
+ContentScalelessComponent.prototype.tag = "ContentScalelessComponent";
+
+
+ContentScalelessComponent.prototype.onCreated = function () {
+    this.$cell = $('.as-component-content-scaleless-cell', this.view);
+    this.$content = this.$cell.childNodes[0];
+    ScalableComponent.prototype.onCreated.call(this);
+};
+
+ContentScalelessComponent.prototype.render = function () {
+    return _({
+        class: 'as-component-content-scaleless',
+
+        child: {
+            class: 'as-component-content-scaleless-cell',
+            child: this.renderContent()
+        }
+    });
+};
+
+ContentScalelessComponent.prototype.renderContent = function(){
+    throw new Error('Not Implement!');
+}
+
+ContentScalelessComponent.prototype.handleStyleTextHAlign = function (value) {
+    this.view.addStyle('text-align', value);
+};
+
+ContentScalelessComponent.prototype.handleStyleTextVAlign = function (value) {
+    if (value == 'center') value = 'middle';
+    this.$cell.addStyle('vertical-align', value);
+};
+
+
+export default ContentScalelessComponent;
