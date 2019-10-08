5fc564c38302da5da4b67be51ad824608c982eee
duthienkt <blueskythien2010@live.com>
2019-10-08 13:40:52 +0700

    - Radio attribute

--- js/components/Radio.js
+++ js/components/Radio.js
@@ -9,6 +9,8 @@ var $ = Fcore.$;
 
 function Radio() {
     ContentScalelessComponent.call(this);
+    
+    
 }
 
 Object.defineProperties(Radio.prototype, Object.getOwnPropertyDescriptors(ContentScalelessComponent.prototype));
@@ -18,14 +20,23 @@ Radio.prototype.menuIcon = "span.mdi.mdi-radiobox-marked";
 
 
 
-Radio.prototype.create = function () {
-    ContentScalelessComponent.prototype.preInit.call(this);
+Radio.prototype.onCreate = function () {
+    ContentScalelessComponent.prototype.onCreate.call(this);
     this.style.width = 18;
     this.style.height = 18;
     this.style.vAlign = 'top';
     this.style.hAlign = 'left';
     this.style.textHAlign = 'center';
     this.style.textVAlign = 'center';
+    this.style.checked = false;
+};
+
+Radio.prototype.onCreated= function(){
+    ContentScalelessComponent.prototype.onCreated.call(this);
+    var self = this;
+    this.$content.on('change', function(){
+        self.attributes.checked = this.checked;
+    });
 };
 
 
@@ -36,7 +47,7 @@ Radio.prototype.renderContent = function () {
 
 Radio.prototype.setAttributeChecked = function (value) {
     this.$content.checked = !!value;
-    return this.$content;
+    return this.$content.checked;
 };
 
 Radio.prototype.setStyleWidth = function (value) {
@@ -49,6 +60,32 @@ Radio.prototype.setStyleHeight = function (value) {
     value = value >= 18 ? value : 18;
     return ContentScalelessComponent.prototype.setStyleHeight.call(this, value);
 };
+
+
+Radio.prototype.setAttributeGroupName = function(value){
+    this.$content.attr('name', value);
+    return value;
+};
+
+Radio.prototype.getAttributeGroupNameDescriptor = function(){
+    return {
+        type: "text",
+        regex:/[a-zA-Z0-9\_\-]+/
+    };
+};
+
+
+Radio.prototype.getAttributeCheckedDescriptor = function(){
+    return {
+        type: "bool"
+    };
+};
+
+
+Radio.prototype.getAcceptsAttributeNames = function(){
+    return ContentScalelessComponent.prototype.getAcceptsAttributeNames.call(this).concat(["groupName", "checked"])
+};
+
 
 
 export default Radio;
