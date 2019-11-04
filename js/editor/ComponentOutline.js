5ab32e19bdc20154d5220a5435c156128d4029fa
duthienkt <blueskythien2010@live.com>
2019-11-04 17:32:30 +0700

    - select on Outline

--- js/editor/ComponentOutline.js
+++ js/editor/ComponentOutline.js
@@ -2,6 +2,7 @@ import Fcore from "../core/FCore";
 import EventEmitter from "absol/src/HTML5/EventEmitter";
 import Context from "absol/src/AppPattern/Context";
 import R from "../R";
+import '../../css/componentoutline.css';
 
 var _ = Fcore._;
 var $ = Fcore.$;
@@ -16,6 +17,9 @@ function ComponentOutline() {
      */
     this.mLayoutEditor = null;
     this.activeComponents = [];
+    this.$expNodes = [];
+    this.$focusNode = undefined;
+    this._lastPressTime = 0;
 }
 
 Object.defineProperties(ComponentOutline.prototype, Object.getOwnPropertyDescriptors(Context.prototype));
@@ -29,9 +33,24 @@ ComponentOutline.prototype.onStart = function () {
 
 ComponentOutline.prototype.ev_contextNode = function (comp, event) {
     var self = this;
-    event.stopPropagation();
-    event.showContextMenu({
-        items: [
+    var items = [{
+        icon: 'span.mdi.mdi-delete-variant',
+        text: 'Delete',
+        cmd: 'delete',
+        extendStyle: {
+            color: 'red'
+        }
+    }];
+    var anchorEditor = this.mLayoutEditor.findAnchorEditorByComponent(comp);
+    if (anchorEditor) {
+        anchorEditor.focus()
+    }
+    else {
+        this.mLayoutEditor.setActiveComponent(comp);
+    }
+
+    if (this.mLayoutEditor.anchorEditors.length == 1) {
+        items = [
             {
                 icon: 'span.mdi.mdi-arrow-collapse-up',
                 text: 'Move To Top',
@@ -53,16 +72,13 @@ ComponentOutline.prototype.ev_contextNode = function (comp, event) {
                 cmd: 'move-to-bottom'
             },
             '=================',
-            {
-                icon: 'span.mdi.mdi-delete-variant',
-                text: 'Delete',
-                cmd: 'delete',
-                extendStyle: {
-                    color: 'red'
-                }
-            }
 
-        ]
+
+        ].concat(items)
+    }
+    event.stopPropagation();
+    event.showContextMenu({
+        items: items
     }, function (event) {
         switch (event.menuItem.cmd) {
             case "delete":
@@ -87,56 +103,37 @@ ComponentOutline.prototype.ev_contextNode = function (comp, event) {
 };
 
 ComponentOutline.prototype.moveToTop = function (comp) {
-    var parent = comp.parent;
-    if (!parent) return;
-    var firstChild = parent.children[0];
-    if (firstChild == comp) return;
-    comp.remove();
-    parent.addChildBefore(comp, firstChild);
-    this.updateComponetTree();
-    this.mLayoutEditor.notifyDataChange();
+    this.mLayoutEditor.moveToTopComponent(comp);
 };
 
 ComponentOutline.prototype.moveUp = function (comp) {
-    var parent = comp.parent;
-    if (!parent) return;
-    var prevChild = parent.findChildBefore(comp);
-    if (!prevChild) return;
-    comp.remove();
-    parent.addChildBefore(comp, prevChild);
+    this.mLayoutEditor.moveUpComponent(comp);
     this.updateComponetTree();
-    this.mLayoutEditor.notifyChange();
 };
 
 ComponentOutline.prototype.moveDown = function (comp) {
-    var parent = comp.parent;
-    if (!parent) return;
-    var nextChild = parent.findChildAfter(comp);
-    if (!nextChild) return;
-    nextChild.remove();
-    parent.addChildBefore(nextChild, comp);
+    this.mLayoutEditor.moveDownComponent(comp);
     this.updateComponetTree();
-    this.mLayoutEditor.notifyChange();
 }
 
 ComponentOutline.prototype.moveToBottom = function (comp) {
-    var parent = comp.parent;
-    if (!parent) return;
-    comp.remove();
-    parent.addChild(comp);
+    this.mLayoutEditor.moveToBottomComponent(comp);
     this.updateComponetTree();
-    this.mLayoutEditor.notifyDataChange();
 };
 
 ComponentOutline.prototype.updateComponetTree = function () {
     var self = this;
+    this.$expNodes = [];
     if (this.$exptree) {
         this.$exptree.remove();
         this.$exptree = undefined
     }
 
-    function onPressNode() {
-        self.mLayoutEditor.setActiveComponent(this.__comp__);
+    function onPressNode(event) {
+        if (event.shiftKey)
+            self.mLayoutEditor.toggleActiveComponent(this.__comp__);
+        else
+            self.mLayoutEditor.setActiveComponent(this.__comp__);
     }
 
     function visit(expTree, comp) {
@@ -156,6 +153,7 @@ ComponentOutline.prototype.updateComponetTree = function () {
                     }
                 });
                 expTree.addChild(childElt);
+                self.$expNodes.push(childElt);
                 visit(childElt, childComp)
             });
         }
@@ -176,19 +174,56 @@ ComponentOutline.prototype.updateComponetTree = function () {
                 press: onPressNode
             }
         });
-
+        this.$expNodes.push(this.$exptree);
         visit(this.$exptree, this.mLayoutEditor.rootLayout);
         if (this.$view) {
             this.$view.addChild(this.$exptree);
         }
     }
+    this.updateComponentStatus();
+};
+
+ComponentOutline.prototype.updateComponentStatus = function () {
+    this.$focusNode = undefined;
+    var nodeElt;
+    var editor;
+    for (var i = 0; i < this.$expNodes.length; ++i) {
+        nodeElt = this.$expNodes[i];
+        editor = undefined;
+        for (var j = 0; j < this.mLayoutEditor.anchorEditors.length; ++j) {
+            if (this.mLayoutEditor.anchorEditors[j].component == nodeElt.__comp__) {
+                editor = this.mLayoutEditor.anchorEditors[j];
+            }
+            if (editor) {
+                nodeElt.addClass('as-component-outline-node-selected');
+                if (editor.isFocus) {
+                    nodeElt.addClass('as-component-outline-node-focus');
+                    this.$focusNode = nodeElt;
+                }
+                else {
+                    nodeElt.removeClass('as-component-outline-node-focus');
+                }
+
+            }
+            else {
+                nodeElt.removeClass('as-component-outline-node-selected');
+                nodeElt.removeClass('as-component-outline-node-focus');
+            }
+        }
+    }
+    // console.log(this.mLayoutEditor.getActivatedComponents());
 };
 
 ComponentOutline.prototype.getView = function () {
     if (this.$view) return this.$view;
     this.$view = _({
-        class: 'as-component-outline'//,
-        // child:{}
+        class: 'as-component-outline',
+        attr: {
+            tabindex: '1'
+        },
+        on: {
+            keydown: this.ev_keydown.bind(this)
+        }
     });
     if (this.$exptree)
         this.$view.addChild(this.$exptree);
@@ -197,7 +232,53 @@ ComponentOutline.prototype.getView = function () {
 };
 
 
+ComponentOutline.prototype.ev_keydown = function (event) {
+    var now = new Date().getTime();
+    if (now - this._lastPressTime < 50) return;
+    this._lastPressTime = now;
+    switch (event.key) {
+        case 'Down':
+        case 'ArrowDown':
+            if (this.$focusNode) this.selectNext(this.$focusNode.__comp__);
+            break;
+        case 'Up':
+        case 'ArrowUp':
+            if (this.$focusNode) this.selectPrev(this.$focusNode.__comp__);
+            break;
+    }
+};
 
+
+ComponentOutline.prototype.selectNext = function (component) {
+    var prev = undefined;
+    var self = this;
+    $('exptree', this.$view, function (node) {
+        if (node.__comp__ == component) {
+            prev = node;
+        }
+        else if (prev) {
+            self.mLayoutEditor.setActiveComponent(node.__comp__);
+            node.$node.focus();
+            return true;
+        }
+    });
+};
+
+ComponentOutline.prototype.selectPrev = function (component) {
+    var prev = undefined;
+    var self = this;
+    $('exptree', this.$view, function (node) {
+        if (node.__comp__ == component) {
+            if (prev) {
+                self.mLayoutEditor.setActiveComponent(prev.__comp__);
+                prev.$node.focus();
+                return true;
+            }
+        }
+        prev = node;
+
+    });
+};
 
 
 export default ComponentOutline;
