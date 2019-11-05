56c0b7702d5d28344d78ca326eb4535a1a233dcb
duthienkt <blueskythien2010@live.com>
2019-11-05 10:15:51 +0700

    - better event for ResizeBox

--- js/dom/ResizeBox.js
+++ js/dom/ResizeBox.js
@@ -10,7 +10,7 @@ var $ = Fcore.$;
 function ResizeBox() {
     var res = _({
         class: 'as-resize-box',
-        extendEvent: ['beginmove', 'endmove', 'moving'],
+        extendEvent: ['beginmove', 'endmove', 'moving', 'click'],//override click event
         child: {
             class: 'as-resize-box-body',
             child: [
@@ -29,7 +29,9 @@ function ResizeBox() {
 
     res.eventHandler = OOP.bindFunctions(res, ResizeBox.eventHandler);
     res.on('mousedown', res.eventHandler.mouseDownBody);
-    res._lastOptionNames = undefined;
+    res._mousedownEventData = undefined;
+    res._mousemoveEventData = undefined;
+
     return res;
 }
 
@@ -39,11 +41,12 @@ ResizeBox.eventHandler.mouseDownBody = function (event) {
     if (EventEmitter.isMouseRight(event)) return;
     event.preventDefault();
     this._optionNames = event.target.attr('class').match(/body|left|top|right|bottom/g);
-    var option =  this._optionNames.reduce(function (ac, key) {
+    var option = this._optionNames.reduce(function (ac, key) {
         ac[key] = true;
         return ac;
     }, {});
-    var beginMoveEvent = {
+
+    this._mousedownEventData = {
         clientX: event.clientX,
         clientY: event.clientY,
         target: this,
@@ -53,77 +56,105 @@ ResizeBox.eventHandler.mouseDownBody = function (event) {
             this.prevented = true;
         },
         option: option,
+        begin: false,
         type: 'beginmove'
     };
 
-    this.emit('beginmove', beginMoveEvent);
-    if (beginMoveEvent.prevented) return;
-    $(document.body).on('mousemove', this.eventHandler.mouseMoveBody)
+    $(document.body).on('mousemove', this.eventHandler.mouseMoveBody);
+    $(document.body)
         .on('mouseup', this.eventHandler.mouseFinishBody)
-        .on('mouseleave', this.eventHandler.mouseFinishBody)
-        .addClass('as-resize-box-overiding')
-        .addClass(this._optionNames.join('-'));
+        .on('mouseleave', this.eventHandler.mouseFinishBody);
 
 };
 
 ResizeBox.eventHandler.mouseMoveBody = function (event) {
-    event.preventDefault();
-    var beginMoveEvent = {
-        clientX: event.clientX,
-        clientY: event.clientY,
-        target: this,
-        originEvent: event,
-        type: 'moving'
-    };
+    if (this._mousedownEventData && !this._mousedownEventData.begin && !this._mousedownEventData.prevented) {
+        this.emit('beginmove', this._mousedownEventData, this);
+        if (this._mousedownEventData.prevented) {
+            $(document.body).off('mousemove', this.eventHandler.mouseMoveBody);
+        }
+        else {
+            this._mousedownEventData.begin = true;
+            $(document.body)
+                .addClass('as-resize-box-overiding')
+                .addClass(this._optionNames.join('-'));
+        }
 
-    this.emit('moving', beginMoveEvent);
+    }
+
+
+    if (this._mousedownEventData.begin) {
+        event.preventDefault();
+        this._mousemoveEventData = {
+            clientX: event.clientX,
+            clientY: event.clientY,
+            clientX0: this._mousedownEventData.clientX,
+            clientY0: this._mousedownEventData.clientY,
+            clientDX: this._mousedownEventData.clientX - event.clientX,
+            clientDY: this._mousedownEventData.clientY - event.clientY,
+            target: this,
+            originEvent: event,
+            type: 'moving'
+        };
+
+        this.emit('moving', this._mousemoveEventData, this);
+    }
 };
 
 ResizeBox.eventHandler.mouseFinishBody = function (event) {
-    event.preventDefault();
 
     $(document.body).off('mousemove', this.eventHandler.mouseMoveBody)
         .off('mouseup', this.eventHandler.mouseFinishBody)
         .off('mouseleave', this.eventHandler.mouseFinishBody)
         .removeClass('as-resize-box-overiding')
-        .removeClass(this._optionNames.join('-'));;
-    var endMoveEvent = {
-        clientX: event.clientX,
-        clientY: event.clientY,
-        target: this,
-        originEvent: event,
-        type: 'endmove'
-    };
-    this.emit('endmove', endMoveEvent);
+        .removeClass(this._optionNames.join('-'));
     this._optionNames = undefined;
+
+    if (this._mousedownEventData.begin) {
+        this._mousefinishEventData = {
+            clientX: event.clientX,
+            clientY: event.clientY,
+            clientX0: this._mousedownEventData.clientX,
+            clientY0: this._mousedownEventData.clientY,
+            clientDX: this._mousedownEventData.clientX - event.clientX,
+            clientDY: this._mousedownEventData.clientY - event.clientY,
+            target: this,
+            originEvent: event,
+            type: 'endmove'
+        };
+        this.emit('endmove', this._mousefinishEventData, this);
+    }
+    else {
+        this.emit('click', event, this);
+    }
 };
 
 
 ResizeBox.property = {};
 
 ResizeBox.property.canMove = {
-    set:function(value) {
-        if (value){
+    set: function (value) {
+        if (value) {
             this.addClass('as-can-move');
         }
-        else{
+        else {
             this.removeClass('as-can-move');
         }
-    }, 
+    },
     get: function () {
         return this.containsClass('as-can-move');
     }
 }
 
 ResizeBox.property.canResize = {
-    set:function(value) {
-        if (value){
+    set: function (value) {
+        if (value) {
             this.addClass('as-can-resize');
         }
-        else{
+        else {
             this.removeClass('as-can-resize');
         }
-    }, 
+    },
     get: function () {
         return this.containsClass('as-can-resize');
     }
