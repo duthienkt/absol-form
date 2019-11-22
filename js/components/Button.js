0b9c551de48027c2c4a9317deb87a38ba9d6c84a
duthienkt <blueskythien2010@live.com>
2019-11-22 15:51:47 +0700

    - button

--- js/components/Button.js
+++ js/components/Button.js
@@ -16,33 +16,44 @@ Button.prototype.menuIcon = "span.mdi.mdi-alpha-b-box";
 Button.prototype.onCreate = function () {
     ScalableComponent.prototype.onCreate.call(this);
     this.attributes.text = this.attributes.name;
+    this.attributes.icon = 'span.mdi.mdi-format-font';
 };
 
 Button.prototype.onCreated = function () {
     ScalableComponent.prototype.onCreated.call(this);
     var self = this;
-    this.view.on('click', function(event){
-        if (self.events.click){
-            console.log('TODO: exec', self.events.click);
-        }
+    this.view.on('click', function (event) {
+        self.emit('click', { type: 'click', target: this, originEvent: event.originEvent || event }, self)
     });
 };
 
 
-
 Button.prototype.render = function () {
-    return _('button');
+    return _('flexiconbutton');
+};
+
+
+Button.prototype.setStyleHeight = function (value) {
+    var res = ScalableComponent.prototype.setStyleHeight.call(this, value);
+    this.view.childNodes[0].addStyle('height', value - 2 + 'px');
+    return res;
 };
 
 
 Button.prototype.setAttributeText = function (value) {
-    this.view.clearChild().addChild(_({ text: value }));
+    this.view.text = value;
+    return value;
+};
+
+
+Button.prototype.setAttributeIcon = function (value) {
+    this.view.icon = value;
     return value;
 };
 
 
 Button.prototype.getAcceptsAttributeNames = function () {
-    return ScalableComponent.prototype.getAcceptsAttributeNames.call(this).concat(['text']);
+    return ScalableComponent.prototype.getAcceptsAttributeNames.call(this).concat(['text', 'icon']);
 };
 
 
@@ -51,5 +62,19 @@ Button.prototype.getAttributeTextDescriptor = function () {
         type: "text",
     };
 };
+
+
+Button.prototype.getAttributeIconDescriptor = function () {
+    return {
+        type: "icon"
+    };
+};
+
+
+Button.prototype.measureMinSize = function () {
+    var fontSize = this.view.getFontSize();
+    return { width: fontSize * 2 + 2, height: fontSize * 2 + 2 }
+};
+
 
 export default Button;
