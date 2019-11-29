9b22ea6716581d3cfc6ee436957bf36503fc9a32
duthienkt <blueskythien2010@live.com>
2019-11-29 22:16:16 +0700

    + logo icon

--- js/dom/Icons.js
+++ js/dom/Icons.js
@@ -42,3 +42,18 @@ Fcore.install('mdi-align-vertical-top', function () {
            <path fill="#000000" d="M11 22H5V6H11V22M19 6H13V16H19V6M22 2H2V4H22V2Z" />\
         </svg>');
 });
+
+Fcore.install('frame-ico', function () {
+    return _(
+        '<svg width="25.03mm" height="26.1mm" version="1.1" viewBox="0 0 25.03 26.1" xmlns="http://www.w3.org/2000/svg">' +
+        '    <g transform="translate(0,-270.9)">' +
+        '        <path d="m12.662 270.91-7.0158 3.7418v4.0759l6.9156-4.0424 7.0492 3.9756-0.13363-3.9422z" style="fill:gray" />' +
+        '        <path d="m12.628 276.89-6.7486 3.6416 6.6817 4.0424 6.7151-3.842z" style="fill:rgb(240, 122, 0)" />' +
+        '        <path d="m5.2452 282.4-0.03341 7.3165 6.3811 3.8754 0.03341-7.4167z" style="fill:rgb( 122, 240, 0)" />' +
+        '        <path d="m19.745 282.37-6.3811 3.842v7.3499l6.4145-3.7084z" style="fill:rgb(0, 122, 240)" />' +
+        '        <path d="m21.816 283.07-0.13364 7.8176-6.7486 4.1093 3.2406 2.0045 6.7152-3.9088 0.13363-8.1517z" style="fill:gray" />' +
+        '        <path d="m3.1738 283.04-3.1738 2.0045v7.7842l6.7151 4.1093 3.3409-1.8709-6.7151-3.9756z" style="fill:gray" />' +
+        '    </g>' +
+        '</svg>'
+    );
+});
