35633be963b0cec4779e47d6064ca571c64c3ad7
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-01-03 15:17:45 +0700

    - handle by anchor

--- js/core/ScalableComponent.js
+++ js/core/ScalableComponent.js
@@ -27,89 +27,11 @@ ScalableComponent.prototype.onCreate = function () {
 };
 
 
-
-ScalableComponent.prototype.setStyleWidth = function (value) {
-    if (this.style.hAlign != 'fixed')
-        this.view.addStyle('width', value + 'px');
-    else
-        this.view.removeStyle('width');
-    return value;
-};
-
-ScalableComponent.prototype.setStyleHeight = function (value) {
-    if (this.style.vAlign != 'fixed')
-        this.view.addStyle('height', value + 'px');
-    else
-        this.view.removeStyle('height');
-
-    return value;
-};
-
-
-ScalableComponent.prototype.setStyleHAlign = function (value) {
-    if (value != 'fixed') {
-        this.view.addStyle('width', this.style.width + 'px');
-    }
-    else {
-        this.view.removeStyle('width');
-    }
-
-    return value;
-};
-
-
-ScalableComponent.prototype.setStyleVAlign = function (value) {
-    if (value != 'fixed') {
-        this.view.addStyle('height', this.style.height + 'px');
-    }
-    else {
-        this.view.removeStyle('height');
-    }
-    return value;
-};
-
-
-ScalableComponent.prototype.setStyle = function (key, value) {
-    var res;
-    if (this.anchorAcceptsStyleName[key]) {
-        value = this.anchor.setStyle(key, value);// anchor first
-        value = BaseComponent.prototype.setStyle.call(this, key, value);
-        if (value === undefined)
-            delete this.style[key];
-        else this.style[key] = value;
-        res = value;
-    }
-    else {
-        res = BaseComponent.prototype.setStyle.call(this, key, value);
-    }
-
-    return res;
-};
-
-
 ScalableComponent.prototype.getAcceptsStyleNames = function () {
     return BaseComponent.prototype.getAcceptsStyleNames.call(this).concat(['width', 'height']);
 };
 
 
-ScalableComponent.prototype.getStyleWidthDescriptor = function () {
-    return {
-        disabled: this.style.hAlign == 'fixed',
-        type: 'number',
-        min: this.measureMinSize().width,
-        max: Infinity,
-        livePreview: true
-    };
-};
 
-ScalableComponent.prototype.getStyleHeightDescriptor = function () {
-    return {
-        disabled: this.style.vAlign == 'fixed',
-        type: 'number',
-        min: this.measureMinSize().height,
-        max: Infinity,
-        livePreview: true
-    };
-};
 
 export default ScalableComponent;
