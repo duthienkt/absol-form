7dc8c7d087d0f7ca02ec460e3ef2e2ebe0577bfe
Pham Quoc Du Thien <blueskythien2010@live.com>
2020-02-13 14:27:24 +0700

    - edit multi property

--- js/editor/AllPropertyEditor.js
+++ js/editor/AllPropertyEditor.js
@@ -1,70 +1,79 @@
-import PropertyEditor from "./PropertyEditor";
+import MultiObjectPropertyEditor from "../propertyeditors/MultiObjectPropertyEditor";
 
 function AllPropertyEditor() {
-    PropertyEditor.call(this);
-    this.propertyGroup = {};
+    MultiObjectPropertyEditor.call(this);
 }
 
-Object.defineProperties(AllPropertyEditor.prototype, Object.getOwnPropertyDescriptors(PropertyEditor.prototype));
+Object.defineProperties(AllPropertyEditor.prototype, Object.getOwnPropertyDescriptors(MultiObjectPropertyEditor.prototype));
 AllPropertyEditor.prototype.constructor = AllPropertyEditor;
 
 
-AllPropertyEditor.prototype.setProperty = function (name, value) {
-    if (this.propertyGroup.attributes[name])
-        return this.object.setAttribute(this.propertyGroup.attributes[name], value);
-    if (this.propertyGroup.style[name])
-        return this.object.setStyle(this.propertyGroup.style[name], value);
+AllPropertyEditor.prototype.setProperty = function (object, name, value) {
+    if (object.__AllPropertyEditorGroupName__.attributes[name])
+        return object.setAttribute(object.__AllPropertyEditorGroupName__.attributes[name], value);
+    if (object.__AllPropertyEditorGroupName__.style[name])
+        return object.setStyle(object.__AllPropertyEditorGroupName__.style[name], value);
     throw new Error('not found ' + name);
 
 };
 
 
-AllPropertyEditor.prototype.getProperty = function (name) {
-    if (this.propertyGroup.attributes[name])
-        return this.object.getAttribute(this.propertyGroup.attributes[name]);
-    if (this.propertyGroup.style[name])
-        return this.object.getStyle.apply(this.object, [this.propertyGroup.style[name]].concat(Array.prototype.slice.call(arguments, 1)));
+AllPropertyEditor.prototype.getProperty = function (object, name) {
+    if (object.__AllPropertyEditorGroupName__.attributes[name])
+        return object.getAttribute(object.__AllPropertyEditorGroupName__.attributes[name]);
+    if (object.__AllPropertyEditorGroupName__.style[name])
+        return object.getStyle.apply(object, [object.__AllPropertyEditorGroupName__.style[name]].concat(Array.prototype.slice.call(arguments, 1)));
     throw new Error('not found ' + name);
 };
 
 
-AllPropertyEditor.prototype.getPropertyDescriptor = function (name) {
-    if (this.propertyGroup.attributes[name])
-        return this.object.getAttributeDescriptor(this.propertyGroup.attributes[name]);
-    if (this.propertyGroup.style[name])
-        return this.object.getStyleDescriptor(this.propertyGroup.style[name]);
+AllPropertyEditor.prototype.getPropertyDescriptor = function (object, name) {
+    if (object.__AllPropertyEditorGroupName__.attributes[name])
+        return object.getAttributeDescriptor(object.__AllPropertyEditorGroupName__.attributes[name]);
+    if (object.__AllPropertyEditorGroupName__.style[name])
+        return object.getStyleDescriptor(object.__AllPropertyEditorGroupName__.style[name]);
     throw new Error('not found ' + name);
 };
 
 
 
 
-AllPropertyEditor.prototype.getPropertyNames = function () {
-    var attributes = this.object.getAcceptsAttributeNames();
-    var style = this.object.getAcceptsStyleNames();
+AllPropertyEditor.prototype.getPropertyNames = function (object) {
+    return (Object.keys(object.__AllPropertyEditorGroupName__.attributes).concat(Object.keys(object.__AllPropertyEditorGroupName__.style))).sort();
+};
 
-    this.propertyGroup.attributes = attributes.reduce(function (ac, cr) {
-        ac[cr] = cr;
-        return ac;
-    }, {});
-    this.propertyGroup.style = style.reduce(function (ac, cr) {
-        ac[cr] = cr;
-        return ac;
-    }, {});
 
-    var key;
-    for (key in this.propertyGroup.attributes) {
-        if (this.propertyGroup.style[key]) {
-            delete this.propertyGroup.attributes[key];
-            delete this.propertyGroup.style[key];
-            this.propertyGroup.attributes[key + '(attribute)'] = key;
-            this.propertyGroup.style[key + '(style)'] = key;
+
+AllPropertyEditor.prototype.edit = function () {
+    if (this.objects)
+        this.objects.forEach(function (object) {
+            delete object.__AllPropertyEditorGroupName__;
+        });
+    Array.prototype.forEach.call(arguments, function (object) {
+        object.__AllPropertyEditorGroupName__ = {};
+        var attributes = object.getAcceptsAttributeNames();
+        var style = object.getAcceptsStyleNames();
+
+        object.__AllPropertyEditorGroupName__.attributes = attributes.reduce(function (ac, cr) {
+            ac[cr] = cr;
+            return ac;
+        }, {});
+        object.__AllPropertyEditorGroupName__.style = style.reduce(function (ac, cr) {
+            ac[cr] = cr;
+            return ac;
+        }, {});
+
+        var key;
+        for (key in object.__AllPropertyEditorGroupName__.attributes) {
+            if (object.__AllPropertyEditorGroupName__.style[key]) {
+                delete object.__AllPropertyEditorGroupName__.attributes[key];
+                delete object.__AllPropertyEditorGroupName__.style[key];
+                object.__AllPropertyEditorGroupName__.attributes[key + '(attribute)'] = key;
+                object.__AllPropertyEditorGroupName__.style[key + '(style)'] = key;
+            }
         }
-    }
-
-    return Object.keys(this.propertyGroup.attributes).concat(Object.keys(this.propertyGroup.style)).sort();
-};
-
-
+    });
+    return MultiObjectPropertyEditor.prototype.edit.apply(this, arguments);
+}
 
 export default AllPropertyEditor;
