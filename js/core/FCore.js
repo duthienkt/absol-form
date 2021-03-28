bb8045a68eabc2e0d672261466e83dc3978c9598
duthienkt <blueskythien2010@live.com>
2021-03-28 16:57:20 +0700

    - export _, $

--- js/core/FCore.js
+++ js/core/FCore.js
@@ -3,6 +3,8 @@ import Dom from "absol/src/HTML5/Dom";
 import ColorPicker from "absol-colorpicker";
 
 var Fcore = new Dom();
+export var _ = Fcore._;
+export var $ = Fcore.$;
 
 Fcore.install(AComp.core);
 Fcore.install(ColorPicker.core);
