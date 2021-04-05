f7a1368bda9de037ead20c47c61295a7beaa2aac
duthienkt <blueskythien2010@live.com>
2021-04-05 16:57:34 +0700

    * miss dom module

--- js/core/FCore.js
+++ js/core/FCore.js
@@ -1,11 +1,12 @@
 import AComp from "absol-acomp";
 import Dom from "absol/src/HTML5/Dom";
 import ColorPicker from "absol-colorpicker";
+import install from "absol-acomp/js/dom/install";
 
 var Fcore = new Dom();
+install(Fcore);
 export var _ = Fcore._;
 export var $ = Fcore.$;
 
-Fcore.install(AComp.core);
 Fcore.install(ColorPicker.core);
 export default Fcore;
