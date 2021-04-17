a6334e8cfe4b67f40a45ae972f1adc8357fb1628
duthienkt <blueskythien2010@live.com>
2021-04-17 22:41:29 +0700

    - data binding

--- js/components/ArrayOfFragment.js
+++ js/components/ArrayOfFragment.js
@@ -10,6 +10,8 @@ import {AssemblerInstance} from "../core/Assembler";
  */
 function ArrayOfFragment() {
     ScalableComponent.call(this);
+    this.fragments = [];
+    this._makeArray();
 }
 
 OOP.mixClass(ArrayOfFragment, ScalableComponent);
@@ -27,9 +29,6 @@ ArrayOfFragment.prototype.getAcceptsAttributeNames = function () {
     return ScalableComponent.prototype.getAcceptsAttributeNames.call(this).concat("itemFragmentClass");
 };
 
-// ArrayOfFragment.prototype.setAttributeItemFragmentClass = function (value) {
-//     return value;
-// };
 
 ArrayOfFragment.prototype.getAttributeItemFragmentClassDescriptor = function () {
     return {
@@ -49,10 +48,8 @@ ArrayOfFragment.prototype._makeArray = function () {
     this._dataArr = [];
 
     this._dataArr.push = function () {
-        for (var i = 0; i < arguments.length; ++i) {
-            //todo
-        }
-        return Array.prototype.push.apply(this, arguments);
+        this.splice.apply(this, [this.length,this.length].concat(Array.prototype.slice.apply(arguments)));
+        return arguments.length;
     }
 
 
@@ -64,19 +61,60 @@ ArrayOfFragment.prototype._makeArray = function () {
 
 
     this._dataArr.pop = function () {
-
-        return Array.prototype.pop.apply(this, arguments);
+        if (this.length <= 0) return undefined;
+        var res = this[this.length - 1];
+        this.slice(this.length - 1, this.length);
+        return res;
     };
 
 
     this._dataArr.shift = function () {
-
-        return Array.prototype.shift.apply(this, arguments);
+        if (this.length <= 0) return undefined;
+        var res = this[0];
+        this.slice(0, 1);
+        return res;
     };
 
     this._dataArr.splice = function () {
+        var start;
+        var end;
+        var addedItems;
+        if (arguments.length < 1) start = 0;
+        else start = arguments[0];
+        if (arguments.length < 2)
+            end = this.length;
+        else end = arguments[1];
+        if (start < 0) start = this.length - start;
+        if (end < 0) end = this.length - end;
+        start = Math.max(0, start);
+        end = Math.max(start, end);
+        if (arguments.length > 2)
+            addedItems = Array.prototype.slice.call(arguments, 2);
+        else addedItems = [];
 
-        return Array.prototype.splice.apply(this, arguments);
+        var className = self.getAttribute('itemFragmentClass');
+        var newFragments = addedItems.map(function (item) {
+            var frag = AssemblerInstance.buildFragment({
+                class: className
+            });
+            frag.props = item;
+            return frag;
+        });
+        var endElt = self.fragments[end] && self.fragments[end].domElt;
+        var removedFragments = self.fragments.splice.apply(self.fragments,[start, end].concat(newFragments));
+        removedFragments.forEach(function (frg) {
+            frg.domElt.remove();
+        });
+
+        newFragments.forEach(function (frg) {
+            if (endElt)
+                self.domElt.addChildBefore(frg.domElt, endElt);
+            else self.domElt.addChild(frg.domElt);
+        });
+        var newBindItems = newFragments.map(function (frg) {
+            return frg.props;
+        });
+        return Array.prototype.splice.apply(this, [start, end].concat(newBindItems));
     };
 };
 
@@ -86,11 +124,7 @@ ArrayOfFragment.prototype.getDataBindingDescriptor = function () {
         set: function (value) {
             value = value || [];
             if (!value.slice) value = [];
-            self._clearAllFragment();
-            for (var i = 0; i < value.length; ++i) {
-                self._dataArr.push(value[i]);
-            }
-            //todo
+            self._dataArr.splice.apply(self._dataArr, [0, self._dataArr.length].concat(value));
         },
         get: function () {
             return self._dataArr;
