55d84266b925904facddde01b9228f17571ab0d2
duthienkt <blueskythien2010@live.com>
2021-04-24 16:02:29 +0700

    - install tag

--- js/core/FCore.js
+++ js/core/FCore.js
@@ -2,6 +2,7 @@ import AComp from "absol-acomp";
 import Dom from "absol/src/HTML5/Dom";
 import ColorPicker from "absol-colorpicker";
 import install from "absol-acomp/js/dom/install";
+import ColorPickerButton from "absol-colorpicker/components/ColorPickerButton";
 
 var Fcore = new Dom();
 install(Fcore);
@@ -9,4 +10,5 @@ export var _ = Fcore._;
 export var $ = Fcore.$;
 
 Fcore.install(ColorPicker.core);
+Fcore.install(ColorPickerButton);
 export default Fcore;
