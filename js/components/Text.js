b318a7cda7f342cb7892927a6008a2ff99724187
duthienkt <blueskythien2010@live.com>
2021-05-03 22:58:58 +0700

    - Text style

--- js/components/Text.js
+++ js/components/Text.js
@@ -1,7 +1,8 @@
 import Fcore from "../core/FCore";
 import ScalableComponent from "../core/ScalableComponent";
 import showdown from 'showdown';
-import OOP from "absol/src/HTML5/OOP";
+import {inheritComponentClass} from "../core/BaseComponent";
+import Color from "absol/src/Color/Color";
 
 var _ = Fcore._;
 
@@ -16,10 +17,159 @@ function Text() {
     ScalableComponent.call(this);
 }
 
-OOP.mixClass(Text, ScalableComponent);
+inheritComponentClass(Text, ScalableComponent);
 
 Text.prototype.tag = "Text";
 Text.prototype.menuIcon = "span.mdi.mdi-format-color-text";
+
+Text.prototype.attributeHandlers.text = {
+    set: function (value) {
+        value = (value || '') + '';
+        console.log(value, this.attributes.textDecode)
+        switch (this.attributes.textDecode) {
+            case 'html':
+                this.domElt.innerHTML = value;
+                break;
+            case 'markdown':
+                this.domElt.innerHTML = showDownConverter.makeHtml(value).trim().replace(/(^<p>)|(<\/p>$)/, '');
+                break;
+            default:
+                this.domElt.clearChild().addChild(_({ text: value }));
+        }
+        return value;
+    },
+    descriptor: {
+        type: "text",
+        long: true,
+        sign: "HyperText"
+    }
+};
+
+
+Text.prototype.attributeHandlers.textDecode = {
+    set: function (value) {
+        var ref = arguments[arguments.length - 1];
+        var currentValue = ref.get();
+        if (currentValue !== value) {
+            ref.set(value);
+            this.attributes.text = this.attributes.text + '';
+        }
+        return value;
+    },
+    descriptor: {
+        type: "enum",
+        values: ['none', 'markdown', 'html'],
+        sign: 'TextDecode'
+    }
+};
+
+
+Text.prototype.styleHandlers.textColor = {
+    set: function (value) {
+        var vColor;
+        try {
+            if (value instanceof Color) vColor = value;
+            else if (typeof value === 'string') {
+                vColor = Color.parse(value)
+            }
+            else vColor = new Color([0, 0, 0, 0]);
+        } catch (err) {
+            console.error(err);
+            vColor = new Color([0, 0, 0, 0]);
+        }
+        value = vColor.toString('HEX8');
+        this.domElt.addStyle('color', value);
+        return value;
+    },
+    descriptor: {
+        type: "color",
+        sign: "TextColor"
+    }
+};
+
+Text.prototype.styleHandlers.textSize = {
+    set: function (value) {
+        if (value > 0) {
+            this.domElt.addStyle('font-size', value + 'px');
+        }
+        else {
+            this.domElt.removeStyle('font-size');
+            value = undefined;
+        }
+
+        return value;
+    },
+    descriptor: {
+        type: "FontSize",
+        sign: "FontSize"
+    }
+};
+
+Text.prototype.styleHandlers.font = {
+    set: function (value) {
+        if (value)
+            this.domElt.addStyle('font-family', value);
+        else
+            this.domElt.removeStyle('font-family');
+        return value;
+    },
+    descriptor: {
+        type: "font",
+        sign: 'TextFont'
+    }
+};
+
+Text.prototype.styleHandlers.fontStyle = {
+    set: function (value) {
+        if (!this.fontStyle2DomStyle[value]) value = 'Regular';
+        this.domElt.addStyle(this.fontStyle2DomStyle[value] || this.fontStyle2DomStyle.Regular);
+    },
+    descriptor: {
+        type: "enum",
+        values: ['Regular',
+            'Italic', 'Bold', 'Bold italic'],
+        sign: 'FontStyle'
+    },
+    export: function () {
+        var value = arguments[arguments.length - 1].get();
+        if (value === 'Regular') return undefined;
+        return value;
+    }
+};
+
+Text.prototype.fontStyle2DomStyle = {
+    Regular: {
+        fontWeight: 'normal',
+        fontStyle: 'normal'
+    },
+    Bold: {
+        fontWeight: 'bold',
+        fontStyle: 'normal'
+    },
+    'Bold italic': {
+        fontWeight: 'bold',
+        fontStyle: 'italic'
+    },
+    Italic: {
+        fontWeight: 'normal',
+        fontStyle: 'italic'
+    }
+};
+
+Text.prototype.styleHandlers.textAlign = {
+    set: function (value) {
+        if (['left', 'center', 'right'.indexOf(value) >= 0])
+            this.domElt.addStyle('text-align', value);
+        else
+            this.domElt.addStyle('text-align', 'left');
+        return value;
+    },
+    descriptor: {
+        type: "textAlign",
+        sign: "TextAlign"
+    }
+};
+
 
 Text.prototype.onCreate = function () {
     ScalableComponent.prototype.onCreate.call(this);
@@ -38,149 +188,12 @@ Text.prototype.render = function () {
 };
 
 
-Text.prototype.setAttributeText = function (value) {
-    switch (this.attributes.textDecode) {
-        case 'html':
-            this.view.innerHTML = value;
-            break;
-        case 'markdown':
-            this.view.innerHTML = showDownConverter.makeHtml(value).trim().replace(/(^<p>)|(<\/p>$)/, '');
-            break;
-        default:
-            this.view.clearChild().addChild(_({ text: value }));
-    }
-    return value;
-};
-
-
-Text.prototype.setAttributeTextDecode = function (value) {
-    this.attributes.textDecode = value;
-    this.setAttributeText(this.attributes.text);//update
-    return value;
-};
-
-
-Text.prototype.setStyleFont = function (value) {
-    if (value)
-        this.view.addStyle('font-family', value);
-    else
-        this.view.removeStyle('font-family');
-    return value;
-};
-
-Text.prototype.setStyleFontStyle = function (value) {
-    var styleList = {
-        Regular: {
-            fontWeight: 'normal',
-            fontStyle: 'normal'
-        },
-        Bold: {
-            fontWeight: 'bold',
-            fontStyle: 'normal'
-        },
-        'Bold italic': {
-            fontWeight: 'bold',
-            fontStyle: 'italic'
-        },
-        Italic: {
-            fontWeight: 'normal',
-            fontStyle: 'italic'
-        }
-    }
-    this.view.addStyle(styleList[value] || styleList.Regular);
-    return value;
-};
-
-Text.prototype.setStyleTextSize = function (value) {
-
-    if (value > 0)
-        this.view.addStyle('font-size', value + 'px');
-    else
-        this.view.removeStyle('font-size');
-    return value;
-};
-
-Text.prototype.setStyleTextAlign = function (value) {
-    if (['left', 'center', 'right'.indexOf(value) >= 0])
-        this.view.addStyle('text-align', value);
-    else
-        this.view.addStyle('text-align', 'left');
-    return value;
-};
-
-
-Text.prototype.setStyleTextColor = function (value) {
-    this.view.addStyle('color', value);
-    return value;
-};
-
-
 Text.prototype.getAcceptsAttributeNames = function () {
     return ScalableComponent.prototype.getAcceptsAttributeNames.call(this).concat(['text', 'textDecode']);
 };
 
 Text.prototype.getAcceptsStyleNames = function () {
     return ScalableComponent.prototype.getAcceptsStyleNames.call(this).concat(['font', 'fontStyle', 'textSize', 'textAlign', 'textColor']);
-};
-
-
-Text.prototype.getAttributeTextDescriptor = function () {
-    return {
-        type: "text",
-        long: true,
-        sign: "HyperText"
-    };
-};
-
-
-Text.prototype.getAttributeTextDecodeDescriptor = function () {
-    return {
-        type: "enum",
-        values: ['none', 'markdown', 'html'],
-        sign: 'TextDecode'
-    };
-};
-
-
-Text.prototype.getStyleFontDescriptor = function () {
-    return {
-        type: "font",
-        sign: 'TextFont'
-    };
-};
-
-
-Text.prototype.getStyleFontStyleDescriptor = function () {
-    return {
-        type: "enum",
-        values: ['Regular',
-            'Italic', 'Bold', 'Bold italic'],
-        sign: 'FontStyle'
-    };
-};
-
-Text.prototype.getStyleTextSizeDescriptor = function () {
-    return {
-        type: "number",
-        min: 0,
-        max: 1000,
-        sign: "TextSign"
-    };
-};
-
-
-Text.prototype.getStyleTextAlignDescriptor = function () {
-    return {
-        type: "textAlign",
-        sign: "TextAlign"
-    };
-};
-
-Text.prototype.getStyleTextColorDescriptor = function () {
-    return {
-        type: "color",
-        sign: "TextColor"
-    };
 };
 
 Text.prototype.getDataBindingDescriptor = function () {
@@ -194,6 +207,5 @@ Text.prototype.getDataBindingDescriptor = function () {
         }
     }
 };
-
 
 export default Text;
