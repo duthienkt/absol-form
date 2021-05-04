c626738da7ee1893b3a5925e12396d9cafbb8b68
duthienkt <blueskythien2010@live.com>
2021-05-04 16:23:06 +0700

    - Text attribute

--- js/components/TextInput.js
+++ js/components/TextInput.js
@@ -2,7 +2,7 @@ import Fcore from "../core/FCore";
 import ScalableComponent from "../core/ScalableComponent";
 import '../../css/component.css';
 import Text from "./Text";
-import OOP from "absol/src/HTML5/OOP";
+import {inheritComponentClass} from "../core/BaseComponent";
 
 
 var _ = Fcore._;
@@ -15,7 +15,9 @@ function TextInput() {
     ScalableComponent.call(this);
 }
 
-OOP.mixClass(TextInput, ScalableComponent);
+inheritComponentClass(TextInput, ScalableComponent, Text);
+delete TextInput.prototype.attributeHandlers.text;
+delete TextInput.prototype.attributeHandlers.textDecode;
 
 TextInput.prototype.tag = "TextInput";
 TextInput.prototype.menuIcon = "span.mdi.mdi-textbox";
@@ -23,19 +25,68 @@ TextInput.prototype.menuIcon = "span.mdi.mdi-textbox";
 TextInput.prototype.SUPPORT_ATTRIBUTE_NAMES = ['value'];
 TextInput.prototype.SUPPORT_EVENT_NAMES = ['change'];
 
+TextInput.prototype.attributeHandlers.value = {
+    set: function (value) {
+        this.domElt.value = value;
+    },
+    get: function () {
+        return this.domElt.value;
+    },
+    descriptor: {
+        type: "text",
+        sign: "SimpleText"
+    }
+};
+
+TextInput.prototype.attributeHandlers.placeHolder = {
+    set: function (value) {
+        this.domElt.placeholder = value;
+    },
+    get: function () {
+        return this.domElt.placeholder;
+    },
+    descriptor: {
+        type: "text",
+        sign: "TextPlaceHolder"
+    },
+    export: function () {
+        var ref = arguments[arguments.length - 1];
+        var value = ref.get();
+        return value || undefined;
+    }
+};
+
+
+TextInput.prototype.attributeHandlers.textType = {
+    set: function (value) {
+        if (['normal', 'password'].indexOf(value) < 0) value = 'normal';
+        this.domElt.attr('type', ({ normal: 'text', password: 'password' })[value]);
+        if (value === 'password') this.domElt.attr('autocomplete', 'new-password');
+        else this.domElt.attr('autocomplete', undefined);
+        return value;
+    },
+    descriptor: {
+        type: "enum",
+        values: ['normal', 'password'],
+        sign: 'InputTextType'
+    },
+    export: function () {
+        var ref = arguments[arguments.length - 1];
+        var value = ref.get();
+        return value === 'normal' ? undefined : value;
+    }
+}
 
 TextInput.prototype.onCreated = function () {
     ScalableComponent.prototype.onCreated.call(this);
-    this.bindAttribute( 'value');
     var self = this;
     var lastValue;
-    this.view
+    this.domElt
         .on('keydown', function () {
             lastValue = self.attributes.value;
         })
         .on('keyup', function () {
-            if (this.value != lastValue) {
-                self.attributes.value = this.value;
+            if (this.value !== lastValue) {
                 self.emit('change', this.value, self);
             }
         });
@@ -43,7 +94,7 @@ TextInput.prototype.onCreated = function () {
 
 TextInput.prototype.onCreate = function () {
     ScalableComponent.prototype.onCreate.call(this);
-    this.setStyleTextAlign.textAlign = 'left';
+    this.style.textAlign = 'left';
     this.attributes.value = '';
     this.attributes.placeHolder = '';
     this.style.textType = 'normal';
@@ -60,48 +111,7 @@ TextInput.prototype.render = function () {
 
 
 TextInput.prototype.getAcceptsStyleNames = function () {
-    return ScalableComponent.prototype.getAcceptsStyleNames.call(this).concat(['textAlign', 'font', 'fontStyle', 'textSize']);
-};
-
-
-TextInput.prototype.setStyleFont = Text.prototype.setStyleFont;
-TextInput.prototype.getStyleFontDescriptor = Text.prototype.getStyleFontDescriptor;
-
-TextInput.prototype.setStyleFontStyle = Text.prototype.setStyleFontStyle;
-TextInput.prototype.getStyleFontStyleDescriptor = Text.prototype.getStyleFontStyleDescriptor;
-
-TextInput.prototype.setStyleTextSize = Text.prototype.setStyleTextSize;
-TextInput.prototype.getStyleTextSizeDescriptor = Text.prototype.getStyleTextSizeDescriptor;
-
-TextInput.prototype.setStyleTextAlign = Text.prototype.setStyleTextAlign;
-TextInput.prototype.getStyleTextAlignDescriptor = Text.prototype.getStyleTextAlignDescriptor;
-
-TextInput.prototype.setStyleTextColor = Text.prototype.setStyleTextColor;
-TextInput.prototype.getStyleTextColorDescriptor = Text.prototype.getStyleTextColorDescriptor;
-
-
-TextInput.prototype.getAcceptsStyleNames = function () {
-    return ScalableComponent.prototype.getAcceptsStyleNames.call(this).concat(['font', 'fontStyle', 'textSize', 'textAlign', 'textColor']);
-};
-
-
-TextInput.prototype.setAttributeValue = function (value) {
-    this.view.value = value;
-    return value;
-};
-
-
-TextInput.prototype.setAttributePlaceHolder = function (value) {
-    this.view.attr('placeholder', value);
-    return value;
-};
-
-TextInput.prototype.setAttributeTextType = function (value) {
-    if (['normal', 'password'].indexOf(value) < 0) value = 'normal';
-    this.view.attr('type', ({ normal: 'text', password: 'password' })[value]);
-    if (value == 'password') this.view.attr('autocomplete', 'new-password');
-    else this.view.attr('autocomplete', undefined);
-    return value;
+    return Text.prototype.getAcceptsStyleNames.call(this);
 };
 
 
@@ -110,33 +120,10 @@ TextInput.prototype.getAcceptsAttributeNames = function () {
 };
 
 
-TextInput.prototype.getAttributeValueDescriptor = function () {
-    return {
-        type: "text",
-        sign: "SimpleText"
-    }
-};
-
-TextInput.prototype.getAttributePlaceHolderDescriptor = function () {
-    return {
-        type: "text",
-        sign: "SimpleText"
-    }
-};
-
-
-TextInput.prototype.getAttributeTextTypeDescriptor = function () {
-    return {
-        type: "enum",
-        values: ['normal', 'password'],
-        sign: 'InputTextType'
-    }
-};
-
-
 TextInput.prototype.getAcceptsEventNames = function () {
     return ScalableComponent.prototype.getAcceptsEventNames.call(this).concat(['change']);
 };
+
 
 TextInput.prototype.getDataBindingDescriptor = function () {
     var thisC = this;
@@ -149,6 +136,6 @@ TextInput.prototype.getDataBindingDescriptor = function () {
             return thisC.getAttribute('value');
         }
     };
-}
+};
 
 export default TextInput;
