e83e824537e34e96eda5e0411613762d954d2021
duthienkt <blueskythien2010@live.com>
2021-05-07 14:27:09 +0700

    - ComboBox attribute

--- js/components/ComboBox.js
+++ js/components/ComboBox.js
@@ -1,6 +1,7 @@
 import Fcore from "../core/FCore";
 import ScalableComponent from "../core/ScalableComponent";
 import OOP from "absol/src/HTML5/OOP";
+import {inheritComponentClass} from "../core/BaseComponent";
 
 var _ = Fcore._;
 
@@ -12,10 +13,75 @@ function ComboBox() {
     ScalableComponent.call(this);
 }
 
-OOP.mixClass(ComboBox, ScalableComponent);
+inheritComponentClass(ComboBox, ScalableComponent);
 
 ComboBox.prototype.tag = "ComboBox";
 ComboBox.prototype.menuIcon = 'span.mdi.mdi-arrow-down-drop-circle-outline';
+
+ComboBox.prototype.attributeHandlers.value = {
+    set: function (value) {
+        this.domElt.value = value;
+    },
+    get: function () {
+        return this.domElt.value;
+    },
+    descriptor: {
+        type: 'text'
+    }
+};
+
+ComboBox.prototype.attributeHandlers.list = {
+    set: function (value) {
+        this.domElt.items = value;
+    },
+    get: function () {
+        return this.domElt.items;
+    },
+    export: function () {
+        return this.domElt.items.map(function (item) {
+            return { text: item.text, value: item.value };
+        });
+    },
+    descriptor: {
+        type: 'SelectList'
+    }
+};
+
+
+ComboBox.prototype.attributeHandlers.searchable = {
+    set: function (value) {
+        this.domElt.enableSearch = value;
+    },
+    get: function () {
+        return this.domElt.enableSearch;
+    },
+    descriptor: {
+        type: 'bool'
+    }
+};
+
+
+ComboBox.prototype.attributeHandlers.text = {
+    get: function () {
+        var list = this.attributes.list;
+        var value = this.attributes.value;
+        if (list) {
+            for (var i = 0; i < list.length; ++i)
+                if (list[i].value == value) return list[i].text;
+            return '';
+        }
+        else {
+            return "";
+        }
+    },
+    export: function () {
+        return undefined;
+    },
+    descriptor: {
+        type: 'string'
+    }
+};
+
 
 ComboBox.prototype.onCreate = function () {
     ScalableComponent.prototype.onCreate.call(this);
@@ -26,35 +92,21 @@ ComboBox.prototype.onCreate = function () {
         { text: '3', value: '3' }
     ];
     this.attributes.value = '0';
-    Object.defineProperty(this.attributes, 'text', {
-            get: function () {
-                if (this.list) {
-                    for (var i = 0; i < this.list.length; ++i)
-                        if (this.list[i].value == this.value) return this.list[i].text;
-                    return '';
-                }
-                else {
-                    return "";
-                }
-            }
-        }
-    )
+
 };
 
 
 ComboBox.prototype.onCreated = function () {
     ScalableComponent.prototype.onCreated.call(this);
     var self = this;
-    this.bindAttribute( 'searchable','enableSearch');
     this.view.on('minwidthchange', function (event) {
         if (!(self.style.width > event.value)) {
             self.setStyle('width', event.value);
         }
     }).on('change', function () {
-        self.attributes.value = this.value;
+        // self.attributes.value = this.value;
         self.emit("change", { type: 'change', value: this.value }, self);
     });
-    this.attributes.value = this.view.value;
 };
 
 
@@ -63,55 +115,8 @@ ComboBox.prototype.render = function () {
 };
 
 
-ComboBox.prototype.setAttributeList = function (value) {
-    this.view.items = value;
-    return this.view.items;
-};
-
-ComboBox.prototype.getAttributeList = function () {
-    return this.view.items.map(function (item){
-        return  {text: item.text, value: item.value};
-    });
-};
-
-ComboBox.prototype.setAttributeValue = function (value) {
-    this.view.value = value;
-    return this.view.value;
-};
-
-
-ComboBox.prototype.setAttributeText = function (value) {
-    return value;
-};
-
-
 ComboBox.prototype.getAcceptsAttributeNames = function () {
     return ScalableComponent.prototype.getAcceptsAttributeNames.call(this).concat(["list", 'value', 'text', 'searchable']);
-};
-
-ComboBox.prototype.getAttributeListDescriptor = function () {
-    return {
-        type: 'SelectList'
-    };
-};
-
-ComboBox.prototype.getAttributeValueDescriptor = function () {
-    return {
-        type: 'text'
-    };
-};
-
-ComboBox.prototype.getAttributeTextDescriptor = function () {
-    return {
-        type: 'const',
-        value: this.getAttribute('text')
-    };
-};
-
-ComboBox.prototype.getAttributeSearchableDescriptor = function () {
-    return {
-        type: 'bool'
-    };
 };
 
 
