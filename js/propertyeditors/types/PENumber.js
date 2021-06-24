551fe51d7d6680e01efd3b5e83c6f869cd625bc0
duthienkt <blueskythien2010@live.com>
2021-06-24 08:24:14 +0700

    - support null

--- js/propertyeditors/types/PENumber.js
+++ js/propertyeditors/types/PENumber.js
@@ -19,7 +19,9 @@ PENumber.prototype.attachInput = function () {
     this.$input = _({
             tag: 'numberinput',
             class: 'as-need-update',
-            props: {},
+            props: {
+                value: 0
+            },
             on: {
                 change: function (event) {
                     if (event.by === 'keyup') return;
@@ -32,7 +34,35 @@ PENumber.prototype.attachInput = function () {
             }
         }
     );
-    this.cellElt.addChild(this.$input);
+    this.$null = _({
+        tag: 'checkbox',
+        style: {
+            marginLeft: '5px'
+        },
+        props: {
+            checked: false,
+            text: 'null'
+        },
+        on: {
+            change: function () {
+                self.renewDescriptor();
+                if (this.checked) {
+                    self.$input.disabled = true;
+                    self.setValue(null);
+                }
+                else {
+                    self.$input.disabled = false;
+                    self.$input.value = self.descriptor.defaultValue;
+                    self.setValue(self.descriptor.defaultValue);
+
+                }
+                self.notifyChange();
+                self.notifyStopChange();
+            }
+        }
+    });
+    this.cellElt.addChild(this.$input)
+        .addChild(this.$null);
 };
 
 
@@ -43,11 +73,29 @@ PENumber.prototype.reload = function () {
     this.$input.min = typeof (this.descriptor.min) == 'number' ? this.descriptor.min : -Infinity;
     this.$input.max = typeof (this.descriptor.max) == 'number' ? this.descriptor.max : Infinity;
     var value = this.getValue();
-    if (typeof value !== 'number') {
-        value = this.descriptor.defaultValue;
+    if (this.descriptor.nullable) {
+        this.$null.removeStyle('display');
+        if (!isFinite(value) || value === null || value === undefined || isNaN(value)){
+            this.$input.value = 0;
+            this.$input.disabled = true;
+            this.$null.checked = true;
+        }
+        else{
+            this.$input.disabled = !!this.descriptor.disabled;
+            this.$null.checked = false;
+            this.$input.value = value;
+        }
     }
-    this.$input.value = value;
-    this.$input.disabled = this.descriptor.disabled;
+    else {
+        this.$null.addStyle('display', 'none');
+        if (!isFinite(value) || value === null || value === undefined || isNaN(value)){
+            this.$input.value = 0;
+        }
+        else {
+            this.$input.value = value;
+        }
+        this.$input.disabled = this.descriptor.disabled;
+    }
 };
 
 export default PENumber;
