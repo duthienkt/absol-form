21fd04e2db1970328a2e8d4b79201cf5ad35aded
duthienkt <blueskythien2010@live.com>
2021-06-28 20:22:20 +0700

    - use saveTextAs

--- js/blocks/CBFileDownloader.js
+++ js/blocks/CBFileDownloader.js
@@ -1,8 +1,7 @@
 import BaseBlock from "../core/BaseBlock";
 import inheritComponentClass from "../core/inheritComponentClass";
 import {AssemblerInstance} from "../core/Assembler";
-import {_} from "../core/FCore";
-import {randomUniqueIdent} from "../core/utils";
+import {saveTextAs} from "absol/src/Network/FileSaver";
 
 
 /***
@@ -21,27 +20,8 @@ CBFileDownloader.prototype.tag = "CBFileDownloader";
 
 CBFileDownloader.prototype.pinHandlers.data = {
     receives: function (value) {
-        var a;
-        var blob;
-        var url;
         if (typeof value === 'string') {
-            blob = new Blob([value], { type: 'text/plain', encoding: "UTF-8" });
-        }
-        if (blob) {
-            url = URL.createObjectURL(blob);
-        }
-        if (url) {
-            a = _({
-                tag: 'a',
-                style: { display: 'none' },
-                attr: {
-                    download: this.attributes.fileName || randomUniqueIdent() + '.txt',
-                    target: '_blank',
-                    href: url
-                }
-            }).addTo(document.body);
-            a.click();
-            a.remove();
+           saveTextAs(value, this.attributes.fileName);
         }
     },
     descriptor: {
